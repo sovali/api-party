d956b0ffe82780cfc02fa00ec686968629c06193
Selin Ovali <sovali@purdue.edu>
2017-07-11 10:25:34 -0400

    Added components and styled

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Route,Switch, NavLink} from 'react-router-dom'
 
 class App extends Component {
   render() {
@@ -11,7 +12,16 @@ class App extends Component {
             <h3>Ain't no party like an</h3>
             <h1>API Party</h1>
           </div>
+          <ul className="nav-links">
+            <li>
+              <NavLink to='/github'>Github API</NavLink>
+            </li>
+          </ul>
         </div>
+        <Switch>
+          <Route path='/github' render={() => <h1>GitHub!</h1>} />
+          <Route render={() => <p>To get started, click on one of the links ya goon</p>} />
+        </Switch>
       </div>
     );
   }
